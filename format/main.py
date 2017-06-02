1dbd42a69beaa8c37dfdb0f704a16676498c0bb1
SimonJinaphant <SimonJinaphant@users.noreply.github.com>
2017-06-02 12:11:07 -0400

    Add most services needed for the 2 main components

--- format/main.py
+++ format/main.py
@@ -1,17 +1,57 @@
 import asyncio
 from format.jaus.judp import ConnectedJUDPProtocol, Packet, make_multicast_socket
 from format.jaus import Id, Component
+from format.jaus.services import DiscoveryService, EventsService, LivenessService, \
+    AccessControlService, LocalWaypointDriverService, LocalPoseSensorService, \
+    LocalWaypointListDriverService, VelocityStateSensorService
 
+SUB_SYSTEM = 1
 
 event_loop = asyncio.get_event_loop()
 
-component = Component(123, "a", "b", "c")
+# Platform Management will have node id of 1 and component id of 1
+platform_management_component = \
+    Component(id=Id(subsystem=SUB_SYSTEM, node=1, component=1),
+              name="PlatformManagement",
+              node_name="plat_man_comp",
+              subsystem_name="plat_man_subsys",
+              services=[
+                  #Transport,
+                  DiscoveryService,
+                  EventsService,
+                  LivenessService,
+                  AccessControlService
+              ]
+              )
+
+#Navigation and Reporting Component is arbitrarily assigned node id of 1 and component id of 2
+navigation_reporting_component = \
+    Component(id=Id(subsystem=SUB_SYSTEM, node=1, component=2),
+              name="NavigationReport",
+              node_name="nav_rep_comp",
+              subsystem_name="nav_rep_subsys",
+              services=[
+                #Transport,
+                EventsService,
+                AccessControlService,
+                # Management
+                LivenessService,
+                LocalWaypointDriverService,
+                LocalWaypointListDriverService,
+                VelocityStateSensorService,
+                LocalPoseSensorService
+              ]
+              )
 
 _, protocol = event_loop.run_until_complete(event_loop.create_datagram_endpoint(
     lambda: ConnectedJUDPProtocol(loop=event_loop),
-    sock=make_multicast_socket()))
+    sock=make_multicast_socket(3794)))
 
-connection = protocol.connect(123)
-component.listen(connection)
+# Hook up the Component with the protocol
+plat_man_connection = protocol.connect(Id(subsystem=SUB_SYSTEM, node=1, component=1))
+platform_management_component.listen(plat_man_connection)
+
+nav_rep_connection = protocol.connect(Id(subsystem=SUB_SYSTEM, node=1, component=2))
+navigation_reporting_component.listen(nav_rep_connection)
 
 event_loop.run_forever()
