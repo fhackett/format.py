4583b4651e6ec470a1bf86d6fb3a0473a69e2135
fhackett <fhackett.py@gmail.com>
2017-06-02 09:16:25 -0700

    Add no-op transport service for compliance reasons

--- format/jaus/services.py
+++ format/jaus/services.py
@@ -4,6 +4,7 @@ from .core.events import Service as EventsService
 from .core.list_manager import Service as ListManagerService
 from .core.liveness import Service as LivenessService
 from .core.management import Service as ManagementService
+from .core.transport import Service as TransportService
 
 from .mobility.local_pose_sensor import Service as LocalPoseSensorService
 from .mobility.local_waypoint_driver import Service as LocalWaypointDriverService
